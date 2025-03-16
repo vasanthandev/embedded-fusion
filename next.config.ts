07d4e5ffdbb1faabb6cb54bd7caf1b90e9f16741
vasanthan <vramalingam@gsmorg.com>
2025-03-16 23:37:11 +0530

    path fixe

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   reactStrictMode: true,
-  // basePath:"/embedded-fusion",
-  assetPrefix: "/embedded-fusion/", 
+  basePath: "/embedded-fusion", // ✅ Set this to your repo name
+  assetPrefix: "/embedded-fusion/", // ✅ Set this for static assets
   images: {
     unoptimized: true,
   },
