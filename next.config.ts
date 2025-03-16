d98aa4e9d3f67c87c2f32e5a4b08bfaa39db194a
vasanthan <vramalingam@gsmorg.com>
2025-03-16 21:48:27 +0530

    git ignore

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: "export",
-    reactStrictMode: true
+    distDir:"dist",
+    reactStrictMode: true,
+    images: {
+        unoptimized: true, // Disable image optimization for static exports
+    },
 };
 
 export default nextConfig;
